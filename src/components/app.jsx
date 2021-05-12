91690e18941d27c1efbcf368b1ecf157a78da471
xinglin2016 <lixinglin0829@gmail.com>
2021-05-12 13:28:12 -0400

    add city reducer

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -1,19 +1,12 @@
 import React from 'react';
-import City from '../containers/city.jsx';
-import CityList from '../containers/city_list.jsx';
-// [...]
+import ActiveCity from '../containers/active_city';
+import CityList from '../containers/city_list';
 
-// TODO: remove this line and use Redux
-const cities = [
-  { name: 'Paris', address: '16 Villa Gaudelet, 75011 Paris', slug: 'paris' },
-  { name: 'London', address: '14-22 Elder St, London E1 6BT', slug: 'london' },
-  { name: 'Berlin', address: 'Rudi-Dutschke-Straße 26, 10969 Berlin', slug: 'berlin' },
-];
 
 const App = () => {
   return (
     <div className="app">
-      <CityList cities={cities} />
+      <CityList />
       <ActiveCity />
     </div>
   );
