0b9406f6b0140a77dea72e2bea40ec4045dd8fbc
xinglin2016 <lixinglin0829@gmail.com>
2021-05-13 19:34:30 -0400

    implement city

--- src/containers/city.jsx
+++ src/containers/city.jsx
@@ -1,7 +1,15 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+import selectCity from '../actions/index';
 
-const City = () => {
-
+const City = (props) => {
+  return (
+    <li
+      className="list-group-item"
+      onClick={() => props.selectCity(props.city)}
+    >{props.city.name}</li>
+  );
 };
 
 export default City;
