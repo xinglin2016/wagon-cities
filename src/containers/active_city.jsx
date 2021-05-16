0519e1fdd3ab8519cd322bf798db076df2f88317
xinglin2016 <lixinglin0829@gmail.com>
2021-05-15 21:55:50 -0400

    active city

--- src/containers/active_city.jsx
+++ src/containers/active_city.jsx
@@ -1,7 +1,31 @@
+
 import React from 'react';
+import { connect } from 'react-redux';
 
-const ActiveCity = () => {
+const ActiveCity = (props) => {
+  if (!props.activeCity) {
+    return (
+      <div className="active-city">
+        <p>Select a city...</p>
+      </div>
+    );
+  }
 
+  const url = `https://kitt.lewagon.com/placeholder/cities/${props.activeCity.slug}`;
+
+  return (
+    <div className="active-city">
+      <h3>{props.activeCity.name}</h3>
+      <p>{props.activeCity.address}</p>
+      <img src={url} width="100%" />
+    </div>
+  );
 };
 
-export default ActiveCity;
+function mapStateToProps(state) {
+  return {
+    activeCity: state.activeCity
+  };
+}
+
+export default ActivityCity;
