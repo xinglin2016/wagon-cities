67de2c2896c819c752197ec37bf820f0a677f353
xinglin2016 <lixinglin0829@gmail.com>
2021-06-04 13:29:22 -0400

    import reducer

--- src/index.jsx
+++ src/index.jsx
@@ -3,15 +3,16 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 import { createStore, combineReducers } from 'redux';
+import reducers from './reducers'
 
 // internal modules
 import App from './components/app';
 import '../assets/stylesheets/application.scss';
 
-// State and reducers
-const reducers = combineReducers({
-  changeMe: (state = null, action) => state
-});
+// // State and reducers
+// const reducers = combineReducers({
+//   changeMe: (state = null, action) => state
+// });
 
 // render an instance of the component in the DOM
 ReactDOM.render(
