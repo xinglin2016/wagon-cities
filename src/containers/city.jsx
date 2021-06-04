3e8affea515292114981fb285ff9dae00f5bd7af
xinglin2016 <lixinglin0829@gmail.com>
2021-06-04 13:22:47 -0400

    add function mapdispatch in city

--- src/containers/city.jsx
+++ src/containers/city.jsx
@@ -12,4 +12,8 @@ const City = (props) => {
   );
 };
 
-export default City;
+function mapDispatchToProps(dispatch) {
+  return bindActionCreators({ selectCity }, dispatch);
+}
+
+export default connect(null, mapDispatchToProps)(City);
